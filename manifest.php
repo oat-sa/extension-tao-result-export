e248038caf4edead950d469a1c3ff5c3bb461376
bugalot <jerome@taotesting.com>
2018-12-12 14:55:48 +0100

    Bump version 0.1.2

--- manifest.php
+++ manifest.php
@@ -24,7 +24,7 @@ return [
     'label' => 'Export Tools',
     'description' => 'Extension providing tools dedicated to operations.',
     'license' => 'GPL-2.0',
-    'version' => '0.1.1',
+    'version' => '0.1.2',
     'author' => 'Open Assessment Technologies',
     'requires' => [
         'generis' => '>=6.5.1',
